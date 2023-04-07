7e25910e4d03bbea1416117fe5b31ad3544ba68f
Andrea Delli <andrea.delli2001@gmail.com>
2023-04-07 16:28:14 +0200

    sticky navbar on scroll

--- script.js
+++ script.js
@@ -9,3 +9,14 @@ document.addEventListener("DOMContentLoaded", function () {
         document.querySelector('#sidenavCollapse').children[0].classList.toggle('fa-times');
     });
 });
+
+// Sticky navbar on scroll
+window.onscroll = function () {
+    // If page is scrolled, show navbar as fixed and add logo to it
+    const scroll = document.documentElement.scrollTop || document.body.scrollTop;
+    const scrollThreshold = 64; // 64px, like the height of the sticky navbar
+    document.querySelector('.nav-primary-middle').classList.toggle('hidden', scroll > scrollThreshold);
+    document.querySelector('.nav-primary-bottom').classList.toggle('hidden', scroll > scrollThreshold);
+    document.querySelector('#sticky-navbar').classList.toggle('fixed-top', scroll > scrollThreshold);
+    document.querySelector('#sticky-navbar-logo').classList.toggle('hidden', scroll <= scrollThreshold);
+};
