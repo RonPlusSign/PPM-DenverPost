a7f9d0015818de2640866f53a6d50f2448e759b9
Andrea Delli <andrea.delli2001@gmail.com>
2023-04-16 18:05:28 +0200

    add left sidenav

--- script.js
+++ script.js
@@ -1,8 +1,9 @@
+const scrollThreshold = 64; // 64px, like the height of the sticky navbar
+
 // Sticky navbar on scroll
 window.onscroll = function () {
     // If page is scrolled, show navbar as fixed and add logo to it
     const scroll = document.documentElement.scrollTop || document.body.scrollTop;
-    const scrollThreshold = 64; // 64px, like the height of the sticky navbar
     document.querySelector('.nav-primary-middle').classList.toggle('hidden', scroll > scrollThreshold);
     document.querySelector('.nav-primary-bottom').classList.toggle('hidden', scroll > scrollThreshold);
     document.querySelector('#sticky-navbar').classList.toggle('fixed-top', scroll > scrollThreshold);
@@ -19,12 +20,23 @@ function onUserIconClick(element) {
 
 // Click event for sidebar toggle
 function onSidenavCollapseClick(sidenavCollapseBtn) {
+    if (Array.from(document.querySelector('#sidenav-left').classList).includes('active')) {
+        const scroll = document.documentElement.scrollTop || document.body.scrollTop;
+        if (scroll <= scrollThreshold) {
+            document.querySelector('#sticky-navbar').classList.remove('fixed-top');
+            document.querySelector('#sticky-navbar-logo').classList.add('hidden');
+        }
+    } else {
+        document.querySelector('#sticky-navbar').classList.add('fixed-top');
+        document.querySelector('#sticky-navbar-logo').classList.remove('hidden');
+    }
+
     document.querySelector('#sidenav-left').classList.toggle('active');
+    document.querySelector('body').classList.toggle('overlay');
 
     // Change icon
     sidenavCollapseBtn.children[0].classList.toggle('fa-bars');
     sidenavCollapseBtn.children[0].classList.toggle('fa-times');
-    // TODO: Show left sidenav
 }
 
 function toggleFooterMenu(element) {
