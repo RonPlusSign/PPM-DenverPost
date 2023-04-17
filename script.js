df6390c69e2a86fcf1caca9e2e33eb34eddfa008
Andrea Delli <andrea.delli2001@gmail.com>
2023-04-17 16:08:01 +0200

    working searchbar

--- script.js
+++ script.js
@@ -1,20 +1,20 @@
 const scrollThreshold = 64; // 64px, like the height of the sticky navbar
-
+window.onscroll = stickyNavbarOnScroll;	// Sticky navbar on scroll
 window.onload = function () {
     dateForWeatherWidget();	// Calculate current date for weather widget
 
     // TODO: Cookie with disclaimer
 }
 
-// Sticky navbar on scroll
-window.onscroll = function () {
+function stickyNavbarOnScroll() {
     // If page is scrolled, show navbar as fixed and add logo to it
     const scroll = document.documentElement.scrollTop || document.body.scrollTop;
     document.querySelector('.nav-primary-middle').classList.toggle('hidden', scroll > scrollThreshold);
     document.querySelector('.nav-primary-bottom').classList.toggle('hidden', scroll > scrollThreshold);
-    document.querySelector('#sticky-navbar').classList.toggle('fixed-top', scroll > scrollThreshold);
-    document.querySelector('#sticky-navbar-logo').classList.toggle('hidden', scroll <= scrollThreshold);
-};
+
+    if (scroll > scrollThreshold) showStickyNavbar();
+    else hideStickyNavbar();
+}
 
 // Show right sidenav with login/subscribe and change chevron
 function onUserIconClick(element) {
@@ -26,23 +26,19 @@ function onUserIconClick(element) {
 
 // Click event for sidebar toggle
 function onLeftSidenavToggle(sidenavCollapseBtn) {
-    if (Array.from(document.querySelector('#sidenav-left').classList).includes('active')) {
-        const scroll = document.documentElement.scrollTop || document.body.scrollTop;
-        if (scroll <= scrollThreshold) {
-            document.querySelector('#sticky-navbar').classList.remove('fixed-top');
-            document.querySelector('#sticky-navbar-logo').classList.add('hidden');
-        }
-    } else {
-        document.querySelector('#sticky-navbar').classList.add('fixed-top');
-        document.querySelector('#sticky-navbar-logo').classList.remove('hidden');
-    }
-
-    document.querySelector('#sidenav-left').classList.toggle('active');
-    document.querySelector('body').classList.toggle('overlay');
-
     // Change icon
     sidenavCollapseBtn.children[0].classList.toggle('fa-bars');
     sidenavCollapseBtn.children[0].classList.toggle('fa-times');
+
+    // Close everything else
+    if (isSearchbarOpen()) closeSearchbar();
+
+    // Toggle sidenav
+    if (isLeftSidenavOpen()) closeLeftSidenav();
+    else {
+        showStickyNavbar();
+        openLeftSidenav();
+    }
 }
 
 function toggleFooterMenu(element) {
@@ -78,4 +74,68 @@ function dateForWeatherWidget() {
     // Set date in weather widget
     document.querySelector('#weather-date').innerHTML = `${day}, ${month} ${dayNumber}${cardinalSuffix(dayNumber)} ${year}`;
     document.querySelector('#weather-date-sidenav').innerHTML = `${day}, ${month} ${dayNumber}${cardinalSuffix(dayNumber)} ${year}`;
+}
+
+function toggleSearchbar(element) {
+    // Change icon
+    element.children[0].classList.toggle('fa-search');
+    element.children[0].classList.toggle('fa-times');
+
+    // Close everything else
+    if (isLeftSidenavOpen()) closeLeftSidenav();
+
+    // Toggle searchbar
+    if (isSearchbarOpen()) closeSearchbar();
+    else {
+        showStickyNavbar();
+        openSearchbar();
+    }
+}
+
+function isLeftSidenavOpen() {
+    return Array.from(document.querySelector('#sidenav-left').classList).includes('active');
+}
+
+function isSearchbarOpen() {
+    return Array.from(document.querySelector('#searchbar').classList).includes('active');
+}
+
+function showStickyNavbar() {
+    document.querySelector('#sticky-navbar').classList.add('fixed-top');
+    document.querySelector('#sticky-navbar-logo').classList.remove('hidden');
+}
+
+function hideStickyNavbar() {
+    document.querySelector('#sticky-navbar').classList.remove('fixed-top');
+    document.querySelector('#sticky-navbar-logo').classList.add('hidden');
+}
+
+function closeLeftSidenav() {
+    document.querySelector('#sidenav-left').classList.remove('active');
+    document.querySelector('body').classList.remove('overlay');
+    document.querySelector('#sidenav-left-toggle-btn').children[0].classList.remove('fa-times');
+    document.querySelector('#sidenav-left-toggle-btn').children[0].classList.add('fa-bars');
+    stickyNavbarOnScroll();
+}
+
+function openLeftSidenav() {
+    document.querySelector('#sidenav-left').classList.add('active');
+    document.querySelector('body').classList.add('overlay');
+    document.querySelector('#sidenav-left-toggle-btn').children[0].classList.remove('fa-bars');
+    document.querySelector('#sidenav-left-toggle-btn').children[0].classList.add('fa-times');
+}
+
+function openSearchbar() {
+    document.querySelector('#searchbar').classList.add('active');
+    document.querySelector('body').classList.add('overlay');
+    document.querySelector('#searchbar-toggle-btn').children[0].classList.remove('fa-search');
+    document.querySelector('#searchbar-toggle-btn').children[0].classList.add('fa-times');
+}
+
+function closeSearchbar() {
+    document.querySelector('#searchbar').classList.remove('active');
+    document.querySelector('body').classList.remove('overlay');
+    document.querySelector('#searchbar-toggle-btn').children[0].classList.remove('fa-times');
+    document.querySelector('#searchbar-toggle-btn').children[0].classList.add('fa-search');
+    stickyNavbarOnScroll();
 }
